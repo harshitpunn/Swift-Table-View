39ba84d631b2dc9428f24a010b1b78e4e229f7cb
Harshit Pun <harshitpunn@gmail.com>
2023-04-11 22:00:25 -0700

    Initial Commit

---
+++ Table View Demo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Table View Demo
+//
+//  Created by Harshit Punn on 2023-02-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is the cell for \(indexPath.row)"
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
